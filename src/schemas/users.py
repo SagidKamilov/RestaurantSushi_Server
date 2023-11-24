eb3fad3a718f4fecacb08e9671fc0388fea4cc05
SagidKamilov <89615854074a@gmail.com>
2023-11-24 14:26:09 +0300

    Сделано: Схема данных для пользователя

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -1,13 +1,61 @@
+from typing import Optional
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
 class UserSchema(BaseModel):
     id: int
+    date_registration: datetime.date
+    time_registration: datetime.time
+    date_update: datetime.date
+    time_update: datetime.time
     name: str
+    address: str
+    check_address: int
+    ban: bool
 
     class Config:
         from_attributes = True
 
 
-class UserSchemaAdd(BaseModel):
+class UserAddSchema(BaseModel):
+    id: int
     name: str
+    date_registration: datetime.date
+    time_registration: datetime.time
+    date_update: datetime.date = 0
+    time_update: datetime.time = 0
+    address: Optional[str] = None
+    check_address: Optional[int] = 0
+    ban: Optional[bool] = False
+
+
+class UserDelSchema(BaseModel):
+    id: int
+    date_registration: datetime.date
+    time_registration: datetime.time
+    name: Optional[str]
+    ban: bool
+
+
+class UserBanSchema(BaseModel):
+    id: int
+    date_registration: datetime.date
+    time_registration: datetime.time
+    name: Optional[int]
+    ban: bool
+
+
+class UserUpdateSchema(BaseModel):
+    id: int
+    date_update: datetime.date
+    time_update: datetime.time
+    name: Optional[str]
+    address: Optional[str]
+    check_address: Optional[int]
+    ban: Optional[bool]
+
+
+class UserResponseSchema(BaseModel):
+    id: int
