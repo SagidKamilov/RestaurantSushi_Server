e2f3ca1db65a398e5709dffd7e57061d2e91e35f
SagidKamilov <89615854074a@gmail.com>
2023-12-06 14:23:13 +0300

    инструмент для создания базы данных

---
+++ src/db/create_db.py
@@ -0,0 +1,8 @@
+
+from src.db.async_db import engine
+from src.models.base import Base
+
+
+async def create_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
