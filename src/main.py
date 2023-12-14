df3079ed2caa4757734f0ef403d12c65165c5a5a
SagidKamilov <89615854074a@gmail.com>
2023-12-14 22:04:40 +0300

    Перестроил запуск приложения, ликвидировал костыль

--- src/main.py
+++ src/main.py
@@ -1,19 +1,16 @@
-from asyncio import get_event_loop
-
-from uvicorn import run
 from fastapi import FastAPI
 
 from src.api.routers import routers
 from src.db.create_db import create_database
 
+
 app = FastAPI(title="API для Restaurant of Sushi")
+
+
+@app.on_event('start_up')
+async def create_db():
+    await create_database()
+
 
 for router in routers:
     app.include_router(router=router)
-
-
-if __name__ == "__main__":
-    loop = get_event_loop()
-    loop.run_until_complete(create_database())
-
-    run(app="main:app")
