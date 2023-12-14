3bea176ee8d1151bd0ce0b269ffba328a2225c3d
SagidKamilov <89615854074a@gmail.com>
2023-12-14 22:02:48 +0300

    Изменил получение адреса для базы данных

--- src/db/async_db.py
+++ src/db/async_db.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
-engine = create_async_engine("sqlite+aiosqlite:///sqlite.db")
+from src.config import DB_URL
+
+engine = create_async_engine(DB_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
