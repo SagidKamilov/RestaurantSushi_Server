63037950ddcd2a6a92816bf4b52ba76f00b47968
fos <darkfos82@gmail.com>
2023-12-15 22:10:11 +0300

    Добавил модель History

---
+++ src/models/history.py
@@ -0,0 +1,21 @@
+import json
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.models.base import Base
+from src.schemas.history import HistorySchema
+
+class History(Base):
+    __tablename__ = "History"
+
+    history_id: Mapped[int] = mapped_column(primary_key=True)
+    order: Mapped[json]
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"))
+
+    def to_read_model(self) -> HistorySchema:
+        return HistorySchema(
+            history_id=self.history_id,
+            order=self.order,
+            user_id=self.user_id
+        )
