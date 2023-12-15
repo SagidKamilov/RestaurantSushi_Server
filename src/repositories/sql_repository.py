272d6353acab8c6db0097ad34df16fe2a2235796
fos <darkfos82@gmail.com>
2023-12-15 21:07:03 +0300

    Добавил методы в SQLOrmRepository

--- src/repositories/sql_repository.py
+++ src/repositories/sql_repository.py
@@ -5,17 +5,31 @@ from sqlalchemy.ext.asyncio import AsyncSession
 class SQLOrmRepository:
     model = None
 
-    async def add_one(self):
-        pass
+    def __init__(self, session: AsyncSession):
+        self.session = session
 
-    async def edit_one(self):
-        pass
+    async def add_one(self, data: dict):
+        stmt = insert(self.model).values(**data).returning(self.model.id)
+        result = await self.session.execute(stmt)
+        return result.scalar_one()
 
-    async def find_one(self):
-        pass
+    async def edit_one(self, hidden_id: int, data: dict):
+        stmt = update(self.model).values(**data).filter_by(id=hidden_id).returning(self.model.id)
+        result = await self.session.execute(stmt)
+        return result.scalar_one()
+
+    async def find_one(self, hidden_id: int):
+        stmt = select(self.model).filter_by(id=hidden_id)
+        result = await self.session.execute(stmt)
+        return result.scalar_one().to_read_model()
 
     async def find_all(self):
-        pass
+        stmt = select(self.model)
+        result = await self.session.execute(stmt)
+        result = [record[0].to_read_model() for record in result.all()]
+        return result
 
-    async def delete_one(self):
-        pass
+    async def delete_one(self, hidden_id: int):
+        stmt = delete(self.model).filter_by(id=hidden_id)
+        result = await self.session.execute(stmt)
+        return result.rowcount
