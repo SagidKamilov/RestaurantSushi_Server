8002c9714ae02204bc704157b70fa914487052ed
fos <darkfos82@gmail.com>
2023-12-16 21:55:56 +0300

    Зависимости

--- src/api/admins.py
+++ src/api/admins.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from src.schemas.admins import AdminGetSchema, AdminDeleteSchema, AdminAddSchema
 from src.services.admins import AdminsService
 
-from src.api.dependencies import UOWDep
+from src.api.dependencies import UOWDepends
 
 
 router = APIRouter(
@@ -13,25 +13,25 @@ router = APIRouter(
 
 
 @router.post("")
-async def add_admin(admin: AdminAddSchema, uow: UOWDep):
+async def add_admin(admin: AdminAddSchema, uow: UOWDepends):
     admin_id = await AdminsService.add_admin(uow, admin)
     return admin_id
 
 
 @router.get("")
-async def get_admin(admin: AdminGetSchema, uow: UOWDep):
+async def get_admin(admin: AdminGetSchema, uow: UOWDepends):
     admin_data = await AdminsService.get_admin(uow, admin)
     return admin_data
 
 
 @router.get("")
-async def get_admins(uow: UOWDep):
+async def get_admins(uow: UOWDepends):
     admins_data = await AdminsService.get_admins(uow)
     return admins_data
 
 
 @router.delete("")
-async def delete_admin(admin: AdminDeleteSchema, uow: UOWDep):
+async def delete_admin(admin: AdminDeleteSchema, uow: UOWDepends):
     admin_data = await AdminsService.delete_admin(uow, admin)
     return {"deleted_rows": admin_data}
 
