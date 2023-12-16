53de55d1cc293906d34ee2829dc1789d72dd3272
SagidKamilov <89615854074a@gmail.com>
2023-12-16 21:17:43 +0300

    Исправил ошибку в названии

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -2,5 +2,5 @@ from src.models.users import Users
 from src.repositories.sql_repository import SQLOrmRepository
 
 
-class UserRepository(SQLOrmRepository):
+class UsersRepository(SQLOrmRepository):
     model = Users
