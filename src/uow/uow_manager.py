36bf4ef380e57c07b0edf1e4ceedab000044213f
SagidKamilov <89615854074a@gmail.com>
2023-12-16 21:18:11 +0300

    Добавил менеджер. Внес 2 репозитория

---
+++ src/uow/uow_manager.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+from typing import Type
+
+from src.db.async_db import async_session_maker
+
+from src.repositories.users import UsersRepository
+from src.repositories.admins import AdminsRepository
+
+
+class IUnitOfWork(ABC):
+    admins: Type[AdminsRepository]
+    users: Type[UsersRepository]
+
+    @abstractmethod
+    def __init__(self):
+        ...
+
+    @abstractmethod
+    async def __aenter__(self):
+        ...
+
+    @abstractmethod
+    async def __aexit__(self, *args):
+        ...
+
+    @abstractmethod
+    async def commit(self):
+        ...
+
+    @abstractmethod
+    async def rollback(self):
+        ...
+
+
+class UnitOfWork:
+
+    def __init__(self):
+        self.session_factory = async_session_maker
+
+    async def __aenter__(self):
+        self.session = self.session_factory()
+
+        self.users = UsersRepository(self.session)
+        self.admins = AdminsRepository(self.session)
+
+    async def __aexit__(self, *args):
+        await self.rollback()
+        await self.session.close()
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
