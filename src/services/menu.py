df16377fce1e433a2e249d040fd571e0f06f65c1
fos <darkfos82@gmail.com>
2023-12-17 21:28:02 +0300

    Добавил сервис меню

---
+++ src/services/menu.py
@@ -0,0 +1,34 @@
+from src.schemas.menu import MenuAddSchema, MenuDelSchema, MenuUpdateSchema
+from src.models.menu import Menu
+
+from src.uow.uow_manager import IUnitOfWork
+
+
+class MenuService:
+
+    @staticmethod
+    async def menu_get(menu: MenuAddSchema, uow: IUnitOfWork):
+        menu_data = menu.model_dump()
+        async with uow:
+            menu_data = await uow.menu.add_one(data=menu_data)
+            await uow.commit()
+            return menu_data
+
+
+    @staticmethod
+    async def menu_del(menu: MenuDelSchema, uow: IUnitOfWork):
+        menu_data = menu.model_dump()
+        async with uow:
+            row_count = await uow.menu.delete_one(data=menu_data)
+            await uow.commit()
+            return row_count
+
+
+
+    @staticmethod
+    async def menu_update(menu: MenuUpdateSchema, uow: IUnitOfWork):
+        menu_data = menu.model_dump()
+        async with uow:
+            menu_data = await uow.menu.edit_one(data=menu_data)
+            await uow.commit()
+            return menu_data
