a20bd725debc6afd498cbb54fda423a80a703b94
SagidKamilov <89615854074a@gmail.com>
2023-12-19 22:03:39 +0300

    Добавил await для асинхронных методов

--- src/api/users.py
+++ src/api/users.py
@@ -12,29 +12,29 @@ router = APIRouter(
 
 @router.post("/user")
 async def add_user(user: UserAddSchema, uow: UOWDepends):
-    user_id = UserService().add_user(uow=uow, user=user)
+    user_id = await UserService().add_user(uow=uow, user=user)
     return user_id
 
 
 @router.get("/user")
 async def get_user(user: UserGetSchema, uow: UOWDepends):
-    user = UserService().get_user(uow=uow, user=user)
+    user = await UserService().get_user(uow=uow, user=user)
     return user
 
 
 @router.get("/users")
 async def get_users(uow: UOWDepends):
-    users = UserService().get_users(uow=uow)
+    users = await UserService().get_users(uow=uow)
     return users
 
 
 @router.put("/user")
-async def edit_user(user: UserUpdateSchema, uow: UOWDepends):
-    user = UserService().edit_user(uow=uow, user=user)
+async def edit_user(user: UserGetSchema, user_edit: UserUpdateSchema, uow: UOWDepends):
+    user = await UserService().edit_user(uow=uow, user=user_edit)
     return user
 
 
 @router.delete("/user")
 async def delete_user(user: UserDeleteSchema, uow: UOWDepends):
-    edited_rowcount = UserService().delete_user(uow=uow, user=user)
+    edited_rowcount = await UserService().delete_user(uow=uow, user=user)
     return edited_rowcount
