641a2b211efbf05e795abe569f6fc796bc076297
fos <darkfos82@gmail.com>
2023-12-19 22:04:38 +0300

    Добавил методы в сервис history

---
+++ src/services/history.py
@@ -0,0 +1,28 @@
+from src.schemas.history import HistoryAddSchema, HistoryDeleteSchema, HistoryGetSchema
+from src.uow.uow_manager import IUnitOfWork
+
+
+class HistoryService:
+    @staticmethod
+    async def get_history(history: HistoryGetSchema, uow: IUnitOfWork):
+        history_dict: dict = history.model_dump()
+        async with uow:
+            history_id = uow.history.find_one(data=history)
+            return history_id
+
+
+
+    @staticmethod
+    async def add_history(history: HistoryAddSchema, uow: IUnitOfWork):
+        history_dict: dict = history.model_dump()
+        async with uow:
+            history_dict = await uow.history.add_one(data=history_dict)
+            await uow.commit()
+            return history_dict
+
+    @staticmethod
+    async def delete_history(history: HistoryDeleteSchema, uow: IUnitOfWork):
+        history_dict: dict = history.model_dump()
+        async with uow:
+            history_del = await uow.history.delete_one(data=history_dict)
+            return history_del
