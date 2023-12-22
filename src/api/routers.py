96b6db5bc53d57998bd5ebd72b63ed1c4f5bb7eb
fos <darkfos82@gmail.com>
2023-12-22 19:56:50 +0300

    Добавил роутеры в список

--- src/api/routers.py
+++ src/api/routers.py
@@ -1,8 +1,11 @@
 from src.api.admins import router as admin_router
 from src.api.users import router as user_router
-
+from src.api.menu import router as menu_router
+from src.api.history import router as history_router
 
 all_routers = [
     admin_router,
-    user_router
+    user_router,
+    menu_router,
+    history_router
 ]
