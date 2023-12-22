9fa2b09dfa5f6f69d21544916396043772c4faa3
SagidKamilov <89615854074a@gmail.com>
2023-12-22 21:32:05 +0300

    Исправил ошибку параметра id в вызываемой истории схеме. Заменил тип данных pydantic-схему

--- src/models/history.py
+++ src/models/history.py
@@ -5,19 +5,19 @@ from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.types import JSON
 
 from src.models.base import Base
-from src.schemas.history import HistorySchema
+from src.schemas.history import HistorySchema, Order
 
 
 class History(Base):
     __tablename__ = "History"
 
     id: Mapped[int] = mapped_column(name="history_id", primary_key=True, autoincrement=True)
-    order: Mapped[Dict[int, Any]] = mapped_column(type_=JSON)
+    order: Mapped[Order] = mapped_column(type_=JSON)
     user_id: Mapped[int] = mapped_column(ForeignKey("Users.user_id"))
 
     def to_read_model(self) -> HistorySchema:
         return HistorySchema(
-            history_id=self.id,
+            id=self.id,
             order=self.order,
             user_id=self.user_id
         )
