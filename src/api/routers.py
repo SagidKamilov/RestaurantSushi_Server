091e9353b5d767eed689f2f73bae1b5b2f96e8ed
SagidKamilov <89615854074a@gmail.com>
2023-12-22 20:12:03 +0300

    Подключил роутер history

--- src/api/routers.py
+++ src/api/routers.py
@@ -1,8 +1,10 @@
 from src.api.admins import router as admin_router
 from src.api.users import router as user_router
+from src.api.history import router as history_router
 
 
 all_routers = [
     admin_router,
-    user_router
+    user_router,
+    history_router
 ]
