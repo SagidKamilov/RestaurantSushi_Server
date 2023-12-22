98141f1800b180238828396d0da4435332b31ce4
SagidKamilov <89615854074a@gmail.com>
2023-12-22 20:11:06 +0300

    Обновил поля модели данных

--- src/models/history.py
+++ src/models/history.py
@@ -1,3 +1,5 @@
+from typing import Dict, Any
+
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.types import JSON
@@ -10,8 +12,8 @@ class History(Base):
     __tablename__ = "History"
 
     id: Mapped[int] = mapped_column(name="history_id", primary_key=True, autoincrement=True)
-    order: Mapped[JSON]
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"))
+    order: Mapped[Dict[int, Any]] = mapped_column(type_=JSON)
+    user_id: Mapped[int] = mapped_column(ForeignKey("Users.user_id"))
 
     def to_read_model(self) -> HistorySchema:
         return HistorySchema(
