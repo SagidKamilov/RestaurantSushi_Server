8a3ec9b22f23820995cfdd6b300aa0dc46a1145b
SagidKamilov <89615854074a@gmail.com>
2023-12-22 21:34:49 +0300

    Исправил PEP8. Добавил await для метода get_history

--- src/services/history.py
+++ src/services/history.py
@@ -7,10 +7,8 @@ class HistoryService:
     async def get_history(history: HistoryGetSchema, uow: IUnitOfWork):
         history_dict: dict = history.model_dump()
         async with uow:
-            history_id = uow.history.find_one(data=history)
+            history_id = await uow.history.find_one(data=history_dict)
             return history_id
-
-
 
     @staticmethod
     async def add_history(history: HistoryAddSchema, uow: IUnitOfWork):
