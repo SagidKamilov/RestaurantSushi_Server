de79d114f3f513e78725921ffa85d7239f771d8a
fos <darkfos82@gmail.com>
2023-12-22 19:54:13 +0300

    Добавил АПИ запросы к menu

---
+++ src/api/menu.py
@@ -0,0 +1,34 @@
+from src.services.menu import MenuService
+from src.schemas.menu import MenuDelSchema, MenuAddSchema, MenuUpdateSchema
+from src.api.dependencies import UOWDepends
+
+from fastapi import APIRouter
+
+
+router = APIRouter(
+    prefix="menu",
+    tags=[
+        "Menu"
+    ]
+)
+
+
+@router.get("")
+async def add_menu(menu: MenuAddSchema, uow: UOWDepends):
+    menu_data = await MenuService().add_menu(menu=menu, uow=uow)
+    return menu_data
+
+
+@router.post("")
+async def post_menu(menu: MenuUpdateSchema, uow: UOWDepends):
+    menu_data = await MenuService().update_menu(menu=menu, uow=uow)
+    return menu_data
+
+
+@router.delete("")
+async def del_menu(menu: MenuDelSchema, uow: UOWDepends):
+    menu_data = await MenuService().del_menu(menu=menu, uow=uow)
+    return {"deleted_rows": menu_data}
+
+
+
