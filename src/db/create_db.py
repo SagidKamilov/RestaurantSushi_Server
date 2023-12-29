14b1f51ca9b9baa0b1e4fc49e618aa1a8d5ba43a
SagidKamilov <89615854074a@gmail.com>
2023-12-29 23:50:42 +0300

    Незначительные изменения

--- src/db/create_db.py
+++ src/db/create_db.py
@@ -1,7 +1,7 @@
-from src.db.async_db import engine
+from src.db.async_db import engine_orm
 from src.models.base import Base
 
 
 async def create_database():
-    async with engine.begin() as conn:
+    async with engine_orm.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
