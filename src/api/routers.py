3aee5c95d3f6a88d1d7f85a5f862b4abb140e701
SagidKamilov <89615854074a@gmail.com>
2023-12-29 23:51:47 +0300

    Добавил обработчик для корзины заказов

--- src/api/routers.py
+++ src/api/routers.py
@@ -2,11 +2,13 @@ from src.api.admins import router as admin_router
 from src.api.users import router as user_router
 from src.api.menu import router as menu_router
 from src.api.history import router as history_router
+from src.api.basket import router as basket_router
 
 
 all_routers = [
     admin_router,
     user_router,
     menu_router,
-    history_router
+    history_router,
+    basket_router,
 ]
